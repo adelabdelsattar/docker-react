7235cdc6185a8152914f98e2e19a604e54efebae
adelabdelsattar <adelabdelsattar@gmail.com>
2019-02-10 15:17:16 +0200

    initial commit

---
+++ build/precache-manifest.ce003504794fa1912b3010157b205061.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "75218fa32b1810d375fb",
+    "url": "/static/js/main.75218fa3.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "75218fa32b1810d375fb",
+    "url": "/static/css/main.99317ccc.chunk.css"
+  },
+  {
+    "revision": "1dd2207ddcdc2956ee6f7c7dccf15689",
+    "url": "/index.html"
+  }
+];
